339b4cc3ff7730f32324b8a48efb333dcbfae256
kukulele583 <milospetrovic23082007@email.com>
2021-06-19 19:14:24 +0200

    Popravio linkove 2

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,15 +1,15 @@
 var slike= new Array();
 slike[0]= "https://csgo-stats.com/custom/img/favourite-weapons/3d/ak47.png";
-slike[1]= "https://static.wikia.nocookie.net/cswikia/images/7/77/CSGO_M4A4_Inventory.png/revision/latest?cb=20130813202347";
-slike[2]= "https://static.wikia.nocookie.net/cswikia/images/8/81/CSGO_M4A1-S_Inventory.png/revision/latest?cb=20130824215315";
-slike[3]= "https://static.wikia.nocookie.net/cswikia/images/9/93/CSGO_Galil_AR_Inventory.png/revision/latest?cb=20130813202237";
-slike[4]= "https://static.wikia.nocookie.net/cswikia/images/e/e4/CSGO_FAMAS_Inventory.png/revision/latest?cb=20130813202130";
-slike[5]= "https://static.wikia.nocookie.net/cswikia/images/4/47/CSGO_SSG_08_Inventory.png/revision/latest?cb=20130813202813";
-slike[6]= "https://static.wikia.nocookie.net/cswikia/images/1/16/CSGO_SG_553_Inventory.png/revision/latest?cb=20130813202530";
-slike[7]= "https://static.wikia.nocookie.net/cswikia/images/9/92/CSGO_AUG_Inventory.png/revision/latest?cb=20130813202013";
-slike[8]= "https://static.wikia.nocookie.net/cswikia/images/2/23/CSGO_AWP_Inventory.png/revision/latest?cb=20130813202700";
-slike[9]= "https://static.wikia.nocookie.net/cswikia/images/b/bf/CSGO_SCAR-20_Inventory.png/revision/latest?cb=20130813203014";
-slike[10]= "https://static.wikia.nocookie.net/cswikia/images/3/35/CSGO_G3SG1_Inventory.png/revision/latest?cb=20130813202929";
+slike[1]= "https://static.wikia.nocookie.net/cswikia/images/7/77/CSGO_M4A4_Inventory.png";
+slike[2]= "https://static.wikia.nocookie.net/cswikia/images/8/81/CSGO_M4A1-S_Inventory.png";
+slike[3]= "https://static.wikia.nocookie.net/cswikia/images/9/93/CSGO_Galil_AR_Inventory.png";
+slike[4]= "https://static.wikia.nocookie.net/cswikia/images/e/e4/CSGO_FAMAS_Inventory.png";
+slike[5]= "https://static.wikia.nocookie.net/cswikia/images/4/47/CSGO_SSG_08_Inventory.png";
+slike[6]= "https://static.wikia.nocookie.net/cswikia/images/1/16/CSGO_SG_553_Inventory.png";
+slike[7]= "https://static.wikia.nocookie.net/cswikia/images/9/92/CSGO_AUG_Inventory.png";
+slike[8]= "https://static.wikia.nocookie.net/cswikia/images/2/23/CSGO_AWP_Inventory.png";
+slike[9]= "https://static.wikia.nocookie.net/cswikia/images/b/bf/CSGO_SCAR-20_Inventory.png";
+slike[10]= "https://static.wikia.nocookie.net/cswikia/images/3/35/CSGO_G3SG1_Inventory.png";
 
 var trenutnaSlika = 0;
 var ukupnoSlika= slike.length;
